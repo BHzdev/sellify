840dfbb629f43ecb37fa606530d1d419621c8cbc
BHzdev <bruno_tcc_tc@hotmail.com>
2023-10-01 21:26:56 -0300

    feat: add functionality using the viacep api, we use a function to make the request, with its return the rest of the form is filled with data according to the CEP

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -6,19 +6,19 @@
     <input id="email" name="email" type="email" v-model="email" />
     <label for="senha">Senha</label>
     <input id="senha" name="senha" type="password" v-model="senha" />
+    <label for="cep">Cep</label>
+    <input id="cep" name="cep" type="text" v-model="cep" @keyup="fillCep" />
     <label for="estado">Estado</label>
     <input id="estado" name="estado" type="text" v-model="estado" />
     <label for="cidade">Cidade</label>
     <input id="cidade" name="cidade" type="text" v-model="cidade" />
-    <label for="cep">Cep</label>
-    <input id="cep" name="cep" type="text" v-model="cep" />
     <label for="bairro">Bairro</label>
     <input id="bairro" name="bairro" type="text" v-model="bairro" />
     <label for="rua">Rua</label>
     <input id="rua" name="rua" type="text" v-model="rua" />
     <label for="numero">Numero</label>
     <input id="numero" name="numero" type="text" v-model="numero" />
-    <div>
+    <div class="button">
       <slot></slot>
     </div>
   </form>
@@ -26,9 +26,9 @@
 
 <script>
 import { mapFields } from "@/helpers";
+import { getCep } from "@/services";
 
 export default {
-  name: "UserForm",
   computed: {
     ...mapFields({
       fields: [
@@ -45,6 +45,19 @@ export default {
       base: "usuario",
       mutation: "UPDATE_USUARIO",
     }),
+  },
+  methods: {
+    fillCep() {
+      const cep = this.cep.replace(/\D/g, "");
+      if (cep.length === 8) {
+        getCep(cep).then((response) => {
+          this.estado = response.data.uf;
+          this.cidade = response.data.localidade;
+          this.bairro = response.data.bairro;
+          this.rua = response.data.logradouro;
+        });
+      }
+    },
   },
 };
 </script>
