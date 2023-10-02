ecc25d8f7daddb803181a7fcd71510594470fb0a
BHzdev <bruno_tcc_tc@hotmail.com>
2023-10-02 17:26:32 -0300

    style: styling the userView

--- src/views/user/UserView.vue
+++ src/views/user/UserView.vue
@@ -32,4 +32,44 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.user {
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  max-width: 900px;
+  margin: 20px auto;
+  gap: 20px;
+  padding: 20px;
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 16px;
+  display: block;
+  background: #f4f4f4;
+  margin-bottom: 16px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #701cda;
+  color: white;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  font-family: "Inter", sans-serif;
+  text-align: left;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 500px) {
+  .user {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
