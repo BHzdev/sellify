a81cdc2912b79da59398a67d5c60880637e69ad2
BHzdev <bruno_tcc_tc@hotmail.com>
2023-10-06 17:14:34 -0300

    feat: add UserPurchasesView (show all user purchases)

--- src/views/user/UserPurchasesView.vue
+++ src/views/user/UserPurchasesView.vue
@@ -1,9 +1,61 @@
 <template>
-  <p>Compras Usuario</p>
+  <section>
+    <div v-if="purchases">
+      <h2>Compras</h2>
+      <div class="products" v-for="(purchase, index) in purchases" :key="index">
+        <ProductItem v-if="purchase.produto" :product="purchase.produto">
+          <p class="seller">
+            <span>Vendedor: </span>{{ purchase.vendedor_id }}
+          </p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProductItem from "@/components/ProductItem.vue";
+import { mapState } from "vuex";
+import { api } from "@/services.js";
+
+export default {
+  components: {
+    ProductItem,
+  },
+  data() {
+    return {
+      purchases: null,
+    };
+  },
+  computed: {
+    ...mapState(["usuario", "login"]),
+  },
+  methods: {
+    getPurchases() {
+      api.get(`/transacao?comprador_id=${this.usuario.id}`).then((response) => {
+        this.purchases = response.data;
+      });
+    },
+  },
+  watch: {
+    login() {
+      this.getPurchases();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getPurchases();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  margin-bottom: 32px;
+}
+
+.seller {
+  margin-top: 16px;
+}
+</style>
